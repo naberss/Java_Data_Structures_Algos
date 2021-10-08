8fceb3c1cdc98fb41bc59cbe51f0b5ae6d1469a4
lucas <lucas-berto@hotmail.com>
2021-10-07 21:02:06 -0300

    [#12] - Insertion_Sort
    *Minimal formatting performed in Bubble_Sort algorithm code

--- src/com/naberss/Selection_Sort.java
+++ src/com/naberss/Selection_Sort.java
@@ -1,25 +1,32 @@
 package com.naberss;
 
-public class Bubble_Sort {
+public class Selection_Sort {
+
 
     public static void main(String[] args) {
 
         int[] arr = {5, 10, 15, 547, 8, 6, 3, 1};
 
-        for (int unsortedIndex = (arr.length - 1); unsortedIndex > 0; unsortedIndex--) {
 
-            for (int i = 0; i < unsortedIndex; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    swap(arr, i, (i + 1));
+        for (int LastUnsortedIndex = arr.length - 1; LastUnsortedIndex > 0; LastUnsortedIndex--) {
+
+            int max = 0;
+
+            for (int i = 0; i <= LastUnsortedIndex; i++) {
+
+                if (arr[i] > arr[max]) {
+                    max = i;
                 }
             }
 
+            swap(arr, max, LastUnsortedIndex);
+
         }
+
 
         for (int value : arr) {
             System.out.println(value);
         }
-
 
     }
 
